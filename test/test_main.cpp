91a26168ff0b47be2fd7db4c855a6a243ff2956d
Jiwoo Park <jiwoo_90@naver.com>
2023-08-26 16:06:53 +0900

    Print verbose logs

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,12 +1,21 @@
+#include <iostream>
+
 #include <cert.h>
 #include <curl/curl.h>
 #include <openssl/ssl.h>
 
 #include "load_cert.h"
 
+struct CurlCleaner {
+    CurlCleaner() { curl_global_init(CURL_GLOBAL_ALL); }
+    ~CurlCleaner() { curl_global_cleanup(); }
+} curl_cleaner;
+
 int main()
 {
     // curl example
+    std::cout << "==== curl example ====\n";
+
     static constexpr auto blob = curl_blob {
         const_cast<char*>(kCert),
         sizeof(kCert),
@@ -17,6 +26,7 @@ int main()
     auto* curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_CAINFO_BLOB, &blob);
     curl_easy_setopt(curl, CURLOPT_URL, "https://nghttp2.org/httpbin/get");
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
     if (curl_easy_perform(curl) == CURLE_OK)
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
@@ -27,6 +37,8 @@ int main()
         return -1;
 
     // OpenSSL example
+    std::cout << "==== OpenSSL example ====\n";
+
     auto* ctx = SSL_CTX_new(TLS_method());
     auto success = LoadCert(ctx);
     SSL_CTX_free(ctx);
@@ -34,7 +46,11 @@ int main()
     if (!success)
         return -1;
 
+    std::cout << "Success\n";
+
     // curl + OpenSSL example
+    std::cout << "==== curl + OpenSSL example ====\n";
+
     auto ssl_callback = +[](CURL*, void* ssl_ctx, void*) {
         LoadCert(ssl_ctx);
         return CURLE_OK;
@@ -44,6 +60,7 @@ int main()
     curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_SSL_CTX_FUNCTION, ssl_callback);
     curl_easy_setopt(curl, CURLOPT_URL, "https://nghttp2.org/httpbin/get");
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
     if (curl_easy_perform(curl) == CURLE_OK)
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
